1cd4609c2a44eb3f1b8623267578d13befeaa6b6
VTrofym <v.trof@icloud.com>
2022-10-18 23:13:46 +0300

    added buttons delete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,14 +14,32 @@ class App extends Component {
   state = {
     todos: initialTodos,
   };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
   render() {
     const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = todos.reduce(
+      (total, todo) => (todo.complited ? total + 1 : total),
+      0,
+    );
+
     return (
       <>
         <Counter initialValue={10} />
         <Dropdown />
         <ColorPicker options={colorPickerOptions} />
-        <TodoList todos={todos} />
+        <div>
+          <span>Общее количество: {totalTodoCount}</span>
+          <span>Количество выполненных: { completedTodoCount}</span>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
       </>
     );
   }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './TodoList.css'
 
-const TodoList = ({ todos }) => (
-  <ul>
+const TodoList = ({ todos, onDeleteTodo }) => (
+  <ul className='TodoList'>
     {todos.map(({ id, text }) => (
-    <li key={id}>
-        <p>{text}</p>
-        <button>Удалить</button>
+    <li key={id} className="TodoList__item">
+        <p className='TodoList__text'>{text}</p>
+        <button onClick={() => onDeleteTodo(id)}>Удалить</button>
       </li>
       
     ))}
