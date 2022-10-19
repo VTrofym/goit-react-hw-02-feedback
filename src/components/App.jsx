961139aa12b9acce035e9f082df7e04c2229cd1e
VTrofym <v.trof@icloud.com>
2022-10-19 23:22:58 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,23 @@
 import React, { Component } from 'react';
-
 import Counter from './Counter/Counter';
-// import Dropdown from './Dropdown/Dropdown';
-// import ColorPicker from './ColorPicker/ColorPicker';
-// import TodoList from './TodoList/TodoList';
 import Feedbacks from './Feedbacks/Feedbacks';
 
-// import colorPicker from './dataJson/colorPicker.json'
-import initialTodos from './dataJson/todos.json';
-
-// const colorPickerOptions = colorPicker;
 
 class App extends Component {
   state = {
-    todos: initialTodos,
-  };
-
-  deleteTodo = todoId => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== todoId),
-    }));
+    
   };
 
   render() {
-    const { todos } = this.state;
-
-    const totalTodoCount = todos.length;
-    const completedTodoCount = todos.reduce(
-      (total, todo) => (todo.complited ? total + 1 : total),
-      0,
-    );
-
+    
     return (
       <>
         <h1>Please Leave feedback</h1>
         <Counter initialValue={10} />
-        {/* <Dropdown /> */}
-        {/* <ColorPicker options={colorPickerOptions} /> */}
         <div>
-          <span>Общее количество: {totalTodoCount}</span>
-          <span>Количество выполненных: { completedTodoCount}</span>
+          <span>Общее количество: </span>
+          <span>Количество выполненных:</span>
         </div>
-        {/* <TodoList todos={todos} onDeleteTodo={this.deleteTodo} /> */}
         <Feedbacks/>
       </>
     );
